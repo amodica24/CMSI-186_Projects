234a9991d6245e43fc601f4b7942b132cdb27897
amodica24 <anthony.modica23@gmail.com>
2017-02-23 19:12:46 -0800

    Second commit

--- homework03/HighRoll.java
+++ homework03/HighRoll.java
@@ -1,7 +1,7 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *  File name     :  HighRoll.java
  *  Purpose       :  High Roll Game where the user chooses from six options in rolling a dice. The mission is to get the
- *                   highest score. 
+ *                   highest score.
  *  Author        :  Anthony Modica
  *  Date          :  2017-02-23
  *  Description   :
@@ -15,8 +15,10 @@
  *           -----  ----------  ------------  -----------------------------------------------------------
  *  @version 1.0.0  2017-02-14  B.J. Johnson  Initial writing and release
  *  @version 1.1.0  2017-2-23  Anthony Modica First draft
+ *  @version 1.2.0  2017-2-23  Anthony Modica Second draft
  
- 
+
+
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -24,7 +26,7 @@ import java.io.IOException;
 
 public class HighRoll {
 
-     int HighScore = 0;
+
      private static StringBuffer sb = null;
 
         public static void main( String[] args ) {
@@ -32,13 +34,15 @@ public class HighRoll {
           BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
           DiceSet ds = new DiceSet( 6, 6 );
 
+          int HighScore = 0;
+
           while( true ) {
-            System.out.println( " OPTION 1: ROLL ALL THE DICE" );
-            System.out.println( " OPTION 2: ROLL A SINGLE DIE" );
-            System.out.println( " OPTION 3: CALCULATE THE SCORE FOR THIS SET" );
-            System.out.println( " OPTION 4: SAVE THIS SCORE AS HIGH SCORE" );
-            System.out.println( " OPTION 5: DISPLAY THE HIGH SCORE" );
-            System.out.println( " OPTION 6: ENTER 'Q' TO QUIT THE PROGRAM \n" );
+            System.out.println( " Option 1: ROLL ALL THE DICE" );
+            System.out.println( " Option 2: ROLL A SINGLE DIE" );
+            System.out.println( " Option 3: CALCULATE THE SCORE FOR THIS SET" );
+            System.out.println( " Option 4: SAVE THIS SCORE AS HIGH SCORE" );
+            System.out.println( " Option 5: DISPLAY THE HIGH SCORE" );
+            System.out.println( " Option 6: ENTER 'Q' TO QUIT THE PROGRAM \n" );
 
             System.out.print( ">> " );
             String inputLine = null;
@@ -48,49 +52,46 @@ public class HighRoll {
               if( 0 == inputLine.length() ) {
                 System.out.println("You did not enter a number between 1 and 6!");
                 continue;
-              }
-
-              if (inputLine.charAt(0) == '1') {
+              } else if (inputLine.charAt(0) == '1') {
                 ds.roll();
+                System.out.println("You rolled " + ds.toString());
               }
               if (inputLine.charAt(0) == '2') {
               } else if (inputLine.charAt(0) == '2') {
                 System.out.println( "\n Enter a number between 1 and 6:" );
                 System.out.print(   "\n >>" );
 
-                String inputLine2 = null;
-                inputLine2 = input.readLine();
-
-                if (inputLine2.charAt(0) == '1') {
+                if (inputLine.charAt(0) == '1') {
                   ds.rollIndividual(0);
-                } else if (inputLine2.charAt(0) == '2') {
+                } else if (inputLine.charAt(0) == '2') {
                  ds.rollIndividual(1);
-                } else if (inputLine2.charAt(0) == '3') {
+                } else if (inputLine.charAt(0) == '3') {
                  ds.rollIndividual(2);
-                } else if (inputLine2.charAt(0) == '4') {
+                } else if (inputLine.charAt(0) == '4') {
                  ds.rollIndividual(3);
-                } else if (inputLine2.charAt(0) == '5') {
+                } else if (inputLine.charAt(0) == '5') {
                  ds.rollIndividual(4);
-                } else if (inputLine2.charAt(0) == '6') {
+                } else if (inputLine.charAt(0) == '6') {
                  ds.rollIndividual(5);
                 } else System.out.println( "\n You did not enter a number between 1 and 6!" );
 
                 continue;
 
               } else if (inputLine.charAt(0) == '3') {
-                System.out.println( "Your score of your dice is: " + set.sum() + "\n" );
+                System.out.println( "Your score of your dice is: " + ds.sum() + "\n" );
               } else if (inputLine.charAt(0) == '4') {
                 System.out.println( "Your score is saved" + "\n" );
               } else if (inputLine.charAt(0) == '5') {
+                HighScore = ds.sum();
                 System.out.println( "Your high score is: " + HighScore + "\n" );
               } else if (inputLine.charAt(0) == 'Q') {
                 System.out.println("\n Game Over! \n ");
                 break;
               }
+            }
             catch( IOException ioe ) {
               System.out.println( "IOException" );
             }
           }
         }
       }
-    }
