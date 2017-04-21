730ef907f38967d6c5b7bef1bb84e2beadb9dc82
amodica24 <anthony.modica23@gmail.com>
2017-04-21 13:21:47 -0700

    Update Fibonacci.java
    second draft

--- homework06/Fibonacci.java
+++ homework06/Fibonacci.java
@@ -1,23 +1,23 @@
 public class Fibonacci{
+  private static GinormousInt fib1  = new GinormousInt("0");
+  private static GinormousInt fib2  = GinormousInt.ZERO;
+  private static GinormousInt fib3  = GinormousInt.ONE;
+
   public static GinormousInt fibonacci(int number) {
-    GinormousInt fib1  = new GinormousInt("0");
-    GinormousInt fib2  = new GinormousInt("1");
-    GinormousInt fib3  = null;
-
-
-    if (number == 0|| number == 1) {
+    if (number == 0 || number == 1) {
       return GinormousInt.ONE;
-    }
+    } else {
     for (int i = 1; i < number; i ++) {
       fib1 = fib2.addInt(fib3);
       fib2 = fib3;
       fib3 = fib1;
     }
+  }
     return fib1;
   }
 
   public static void main( String args[] ) {
-    Fibonacci fibonacci = new Fibonacci();
+    // Fibonacci fibonacci = new Fibonacci();
     System.out.println(fibonacci(Integer.parseInt(args[0])));
   }
 }
