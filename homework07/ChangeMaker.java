e38335a6b7f1b500d0a73a0614bdc82558f0226b
amodica24 <anthony.modica23@gmail.com>
2017-05-03 21:16:43 -0700

    Update ChangeMaker.java
    final commit

--- homework07/ChangeMaker.java
+++ homework07/ChangeMaker.java
@@ -15,124 +15,124 @@
  *  1.0.0  2017-04-19  Anthony Modica Initial draft
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-/**
-*   Program to determine the optimal number of change with an input of a number of coins
-*/
-public class ChangeMaker {
+ /**
+  *   Program to determine the optimal number of change with an input of a number of coins.
+  */
 
-/**
-* Main method to run the program
-*  @param args    String Array    Represents the input arguments
-*/
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            printUsage();
-            return;
-        }
+  public class ChangeMaker {
+ /**
+  *   Main method to run the program
+  *   @param args    String Array    Represents the input arguments
+  */
 
-        try {
-
-            String[] denominationStrings = args[0].split(",");
-            int[] denominations = new int[denominationStrings.length];
-
-            for (int i = 0; i < denominations.length; i++) {
-                denominations[i] = Integer.parseInt(denominationStrings[i]);
-                if (denominations[i] <= 0) {
-                    System.out.println("Denominations must all be greater than zero.\n");
-                    printUsage();
-                    return;
-                }
-
-                for (int j = 0; j < i; j++) {
-                    if (denominations[j] == denominations[i]) {
-                        System.out.println("Duplicate denominations are not allowed.\n");
-                        printUsage();
-                        return;
-                    }
-                }
-            }
-
-            int amount = Integer.parseInt(args[1]);
-            if (amount < 0) {
-                System.out.println("Change cannot be made for negative amounts.\n");
-                printUsage();
-                return;
-            }
-
-            Tuple change = makeChangeWithDynamicProgramming(denominations, amount);
-            if (change.isImpossible()) {
-                System.out.println("It is impossible to make " + amount + " cents with those denominations.");
-            } else {
-                int coinTotal = change.total();
-                System.out.println(amount + " cents can be made with " + coinTotal + " coin" +
-                        getSimplePluralSuffix(coinTotal) + " as follows:");
-
-                for (int i = 0; i < denominations.length; i++) {
-                    int coinCount = change.getElement(i);
-                    System.out.println("- "  + coinCount + " " + denominations[i] + "-cent coin" +
-                            getSimplePluralSuffix(coinCount));
-                }
-            }
-        } catch (NumberFormatException nfe) {
-            System.out.println("Denominations and amount must all be integers.\n");
-            printUsage();
-        }
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      printUsage();
+      return;
     }
 
+    try {
+
+      String[] denominationStrings = args[0].split(",");
+      int[] denominations = new int[denominationStrings.length];
+
+      for (int i = 0; i < denominations.length; i++) {
+        denominations[i] = Integer.parseInt(denominationStrings[i]);
+        if (denominations[i] <= 0) {
+          System.out.println("Denominations must all be greater than zero.\n");
+          printUsage();
+          return;
+        }
+
+        for (int j = 0; j < i; j++) {
+          if (denominations[j] == denominations[i]) {
+            System.out.println("Duplicate denominations are not allowed.\n");
+            printUsage();
+            return;
+          }
+        }
+      }
+
+      int amount = Integer.parseInt(args[1]);
+      if (amount < 0) {
+        System.out.println("Change cannot be made for negative amounts.\n");
+        printUsage();
+        return;
+      }
+
+      Tuple change = makeChangeWithDynamicProgramming(denominations, amount);
+      if (change.isImpossible()) {
+        System.out.println("It is impossible to make " + amount + " cents with those denominations.");
+      } else {
+        int coinTotal = change.total();
+        System.out.println(amount + " cents can be made with " + coinTotal + " coin" +
+        getSimplePluralSuffix(coinTotal) + " as follows:");
+        for (int i = 0; i < denominations.length; i++) {
+          int coinCount = change.getElement(i);
+          System.out.println("- "  + coinCount + " " + denominations[i] + "-cent coin" +
+          getSimplePluralSuffix(coinCount));
+        }
+      }
+    } catch (NumberFormatException nfe) {
+      System.out.println("Denominations and amount must all be integers.\n");
+      printUsage();
+    }
+  }
+
    /**
-    * A method where the user inputs a series of arguments which represent a value of currency to find the optimal, or least amount of coins.
-    * @param    denominations   array   containing each argument starting with args[0]
-    * @param    amount          int     the amount of cents
-    * @return                   array
+    *   A method where the user inputs a series of arguments which represent a value of currency to find the optimal, or least amount of coins.
+    *   @param    denominations   array   containing each argument starting with args[0]
+    *   @param    amount          int     the amount of cents
+    *   @return                   array
     */
     public static Tuple makeChangeWithDynamicProgramming(int[] denominations, int amount) {
-          Tuple[][] table = new Tuple[denominations.length][amount + 1];
-          int [] e = new int[denominations.length];
-          for(int i = 0; i < denominations.length; i ++) {
-            for (int j = 0; j < amount + 1; j++){
-              if (j < denominations[i] ) {
-                if (j == 0) {
-                  for (int k = 0; k < denominations.length; k++){
-                    e[k] = 0;
-                  }
-                  table[i][j] = new Tuple(e);
-                } else {
-                  table[i][j] = Tuple.IMPOSSIBLE;
-                }
-              } else {
-                for (int k = 0; k < denominations.length; k++){
-                  e[k] = 0;
-                }
-                e[i] = 1;
-                table[i][j] = new Tuple(e);
-
-                if (table[i][j - denominations[i]] == (Tuple.IMPOSSIBLE)) {
-                  table[i][j] = Tuple.IMPOSSIBLE;
-                } else {
-                  table[i][j] = table[i][j].add(table[i][j-denominations[i]]);
-                }
+      Tuple[][] table = new Tuple[denominations.length][amount + 1];
+      int [] e = new int[denominations.length];
+      for(int i = 0; i < denominations.length; i ++) {
+        for (int j = 0; j < amount + 1; j++){
+          if (j < denominations[i] ) {
+            if (j == 0) {
+              for (int k = 0; k < denominations.length; k++){
+                e[k] = 0;
               }
-                if (i != 0){
-                  if (table[i - 1 ][j] != Tuple.IMPOSSIBLE && table[i][j] == Tuple.IMPOSSIBLE){
-                    table[i][j] = table[i - 1][j];
-                  } else if (table[i][j] != Tuple.IMPOSSIBLE && table[i - 1][j] != Tuple.IMPOSSIBLE) {
-                    if (table[i][j].total() > table[i - 1][j].total()) {
-                      table[i][j] = table[i - 1][j];
-                    }
-                  }
-                }
+              table[i][j] = new Tuple(e);
+            } else {
+              table[i][j] = Tuple.IMPOSSIBLE;
+            }
+          } else {
+            for (int k = 0; k < denominations.length; k++){
+              e[k] = 0;
+            }
+            e[i] = 1;
+            table[i][j] = new Tuple(e);
+
+            if (table[i][j - denominations[i]] == (Tuple.IMPOSSIBLE)) {
+              table[i][j] = Tuple.IMPOSSIBLE;
+            } else {
+              table[i][j] = table[i][j].add(table[i][j-denominations[i]]);
+            }
+          }
+          if (i != 0){
+            if (table[i - 1 ][j] != Tuple.IMPOSSIBLE && table[i][j] == Tuple.IMPOSSIBLE){
+              table[i][j] = table[i - 1][j];
+            } else if (table[i][j] != Tuple.IMPOSSIBLE && table[i - 1][j] != Tuple.IMPOSSIBLE) {
+              if (table[i][j].total() > table[i - 1][j].total()) {
+                table[i][j] = table[i - 1][j];
               }
             }
-            return table[denominations.length-1][amount];
           }
+        }
+      }
+      return table[denominations.length-1][amount];
+    }
 
     private static void printUsage() {
-        System.out.println("Usage: java ChangeMaker <denominations> <amount>");
-        System.out.println("  - <denominations> is a comma-separated list of denominations (no spaces)");
-        System.out.println("  - <amount> is the amount for which to make change");
+      System.out.println("Usage: java ChangeMaker <denominations> <amount>");
+      System.out.println("  - <denominations> is a comma-separated list of denominations (no spaces)");
+      System.out.println("  - <amount> is the amount for which to make change");
     }
 
     private static String getSimplePluralSuffix(int count) {
-        return count == 1 ? "" : "s";
+      return count == 1 ? "" : "s";
     }
-}
+  }
